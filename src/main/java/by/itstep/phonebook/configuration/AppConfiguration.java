fcf225ce49b9689f715490275555bc5c4f5302c0
Teacher <teacher@email.com>
2021-10-18 21:04:58 +0300

    add Spring data

---
+++ src/main/java/by/itstep/phonebook/configuration/AppConfiguration.java
@@ -0,0 +1,11 @@
+package by.itstep.phonebook.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@ComponentScan(basePackages = "by.itstep.phonebook")
+@Import({DatabaseConfiguration.class})
+public class AppConfiguration {
+}
